04c6ce6c7aef0d12c172b66ddad08f81995cab24
vasylhupka <hupkav@gmail.com>
2022-06-06 10:58:48 +0300

    n

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="OnJob">
-              <h2>OnJob</h2>
+              OnJob
             </Link>
             {/* Hamburger menu */}
             <div
